d73ae01aeffae41273692f5b4fb662f114d222c1
hozsocs <hozsocs@gmail.com>
2022-08-12 09:10:51 +0200

    Hibák javítása utáni változat! A teszt továbbra sem megy!

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 'use strict';
     
 
-function handleClick(i=0) {
-    let elements = document.querySelectorAll('.button');
-        elements[i].addEventListener('click', function () {
-            console.log (elements[i].textContent); 
-        })
-}
+const handleClick = () => {
+    const elements = document.querySelectorAll(".button");
+    elements.forEach((el) =>
+      el.addEventListener("click", () => {
+        console.log(el.innerHTML);
+      })
+    );
+  };
 
 for (let i = 0; i < elements.length; i ++) {
     handleClick(i);
